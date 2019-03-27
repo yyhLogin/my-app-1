4b61a2580dd9dde75a935e3046d47c1632be6e8f
yyh <846487248@qq.com>
2019-03-27 08:50:27 +0800

    Signed-off-by: yyh <846487248@qq.com> 添加依赖

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Button } from 'antd'
 import logo from './logo.svg';
 import './App.css';
 
@@ -20,6 +21,7 @@ class App extends Component {
             Learn React
           </a>
         </header>
+        <Button type='primary'>测试</Button>
       </div>
     );
   }
